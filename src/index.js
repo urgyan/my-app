e7a457560d5ab7620aa47439c97880f551e2e901
urgyan <urgyanue195125ece@gmail.com>
2022-01-25 16:03:49 +0530

    login ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastProvider } from 'react-toast-notifications';
+
 import './styles/index.css';
-import {App} from './components';
+import { App } from './components';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
